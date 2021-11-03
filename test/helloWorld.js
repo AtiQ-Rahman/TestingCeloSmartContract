6649feca4c1b7c27c4248168e4fd2d55d97ea980
AtiQ-Rahman <bsse0908@iit.du.ac.bd>
2021-11-03 23:08:00 +0600

    testing Smart Contract

---
+++ test/helloWorld.js
@@ -0,0 +1,30 @@
+const HelloWorld = artifacts.require("HelloWorld");
+
+contract("Hello world test", async => {
+  it("Should return Celo", async () => {
+    const instance = await HelloWorld.deployed();
+    const name = await instance.getName.call();
+    assert.equal(name, 'Celo');
+  });
+  
+});
+contract("Hello world test", async => {
+    it("Should rename to Figment Learn", async () => {
+        const instance = await HelloWorld.deployed();
+        await instance.setName('Figment Learn');
+        const name = await instance.getName.call();
+        assert.equal(name, 'Figment Learn');
+      });
+    
+  });
+  
+  contract("Hello world test", async => {
+    it("Should work with special characters ", async () =>  {
+        const instance = await HelloWorld.deployed();
+        await instance.setName('I ❤️ emojis 🎉');
+        const name = await instance.getName.call();
+        assert.equal(name, 'I ❤️ emojis 🎉');
+      });
+    
+  });
+  
